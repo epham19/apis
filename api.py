0ce2f9ed2bc41b594f99ba27347b8b2654851445
epham19 <36887941+epham19@users.noreply.github.com>
2018-04-30 00:02:25 +1000

    part 4 of the api tutorial

--- api.py
+++ api.py
@@ -1,7 +1,14 @@
-import requests
+import argparse
 import logging
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+import tablib
 
 CPI_DATA_URL = 'http://research.stlouisfed.org/fred2/data/CPIAUCSL.txt'
+
 
 class CPIData:
     """Abstraction of the CPI data provided by FRED.
@@ -51,16 +58,18 @@ class CPIData:
             with open(save_as_file) as fp:
                 return self.load_from_file(fp)
 
-
     def load_from_file(self, fp):
         """Loads CPI data from a given file-like object."""
         # Create temporary variables when iterating over data file.
+        reached_dataset = False
         current_year = None
         year_cpi = []
         for line in fp:
             # Skip until we reach the header line "DATE"
-            while not line.startswith("Date "):
-                pass
+            if not reached_dataset:
+                if line.startswith("Date "):
+                    reached_dataset = True
+                continue
 
             # Remove new-line character at the end of each line
             # and split into list
@@ -108,6 +117,7 @@ class CPIData:
         current_cpi = self.year_cpi[current_year]
 
         return float(price) / year_cpi * current_cpi
+
 
 class GiantbombAPI:
     """
@@ -176,6 +186,7 @@ class GiantbombAPI:
                 yield item
                 counter += 1
 
+
 def is_valid_datset(platform):
     """Filters out datasets that can't be used because they are either
     lacking a release date or an original price. For rendering the output
@@ -196,8 +207,122 @@ def is_valid_datset(platform):
         return False
     return True
 
+
+def generate_plot(platforms, output_file):
+    """Generates a bar chart out of the given platforms and writes
+    the output into the specified file as PNG image.
+
+    """
+    # Convert the platforms in a format that can be attached to the 2 axis
+    # of the bar chart.
+    labels = []
+    values = []
+    for platform in platforms:
+        name = platform['name']
+        adjusted_price = platform['adjusted_price']
+        price = platform['original_price']
+
+        # Skip prices higher than 2000 USD
+        if price > 2000:
+            continue
+
+        # If the platform name is too long, replace it with the abbreviation.
+        if len(name) > 15:
+            name = platform['abbreviation']
+        labels.insert(0, "{0}\n$ {1}\n$ {2}".format(name, price,
+                                                    round(adjusted_price, 2)))
+        values.insert(0, adjusted_price)
+
+    # Define the width of each bar and the size of the resulting graph.
+    width = 0.3
+    ind = np.arange(len(values))
+    fig = plt.figure(figsize=(len(labels) * 1.8, 10))
+
+    # Generate a subplot and put values onto it.
+    ax = fig.add_subplot(1, 1, 1)
+    ax.bar(ind, values, width, align='center')
+
+    # Format the X and Y axis labels. Set the ticks on the x-axis
+    # slightly further apart and give them a slight tilting effect.
+    plt.ylabel('Adjusted price')
+    plt.xlabel('Year / Console')
+    ax.set_xticks(ind + 0.3)
+    ax.set_xticklabels(labels)
+    fig.autofmt_xdate()
+    plt.grid(True)
+
+    plt.savefig(output_file, dpi=72)
+    plt.show(dpi=72)
+
+
+def generate_csv(platforms, output_file):
+    """Writes the given platforms into a CSV file.
+
+    The output_file can either be the path to a file or a file-like object.
+
+    """
+    dataset = tablib.Dataset(headers=['Abbreviation', 'Name', 'Year', 'Price',
+                                      'Adjusted price'])
+    for p in platforms:
+        dataset.append([p['abbreviations'], p['name'], p['year'],
+                        p['original_price'], p['adjusted_price']])
+
+    # If the output_file is a string it represents a path to a file which
+    # we have to open first for writing.
+    if isinstance(output_file, str):
+        with open(output_file, 'w+') as fp:
+            fp.write(dataset.csv)
+    else:
+        output_file.write(dataset.csv)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--giantbomb-api-key', required=True,
+                        help='API key provided by Giantbomb.com')
+    parser.add_argument('--cpi-file', default=os.path.join(os.path.dirname(__file__),
+                                                           'CPIAUCSL.txt'),
+                        help='Path to file containing the CPI data')
+    parser.add_argument('--cpi-data-url', default=CPI_DATA_URL,
+                        help='URL which should be used as CPI data source')
+    parser.add_argument('--debug', default=False, actions='store_true',
+                        help='Increases the output level.')
+    parser.add_argument('--csv-file',
+                        help='Path to the PNG file which should contain the'
+                             'data output')
+    parser.add_argument('--plot-file',
+                        help='Path to the PNG file which should contain the'
+                             'data output')
+    parser.add_argument('--limit', type=int,
+                        help='Number of recent platforms to be considered')
+    opts = parser.parse_args()
+    if not (opts.plotfile or opts.csv_file):
+        parser.error("You have to specify either a --csv-file or --plot-file!")
+    return opts
+
+
 def main():
     """This functiom handles the actual logic of this script."""
+    opts = parse_args()
+
+    if opts.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    cpi_data = CPIData()
+    gb_api = GiantbombAPI(opts.giantbomb_api_key)
+
+    print("Disclaimer: This script uses data provided by FRED, Federal"
+          "Reserve Economic Data, from the Federal Reserve Bank of St. Louis"
+          "and Giantbomb.com:\n- {0}\n- http://www.giantbomb.com/api/\n"
+          .format(CPI_DATA_URL))
+
+    if os.path.exists(opts.cpi_file):
+        with open(opts.cpi_file) as fp:
+            cpi_data.load_from_file(fp)
+    else:
+        cpi_data.load_from_url(opts.cpi_data_url, save_as_file=opts.cpi_file)
 
     # Grab CPI data
 
@@ -210,4 +335,3 @@ def main():
     # Generate graph for adjusted price data
 
     # Generate CSV file to save adjusted price data
-
