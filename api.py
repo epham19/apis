afa0449193d427bd1897248350cb92d294c1b483
epham19 <36887941+epham19@users.noreply.github.com>
2018-04-29 23:06:53 +1000

    finished part 2 of the api tutorial

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 
 CPI_DATA_URL = 'http://research.stlouisfed.org/fred2/data/CPIAUCSL.txt'
 
@@ -107,6 +108,93 @@ class CPIData:
         current_cpi = self.year_cpi[current_year]
 
         return float(price) / year_cpi * current_cpi
+
+class GiantbombAPI:
+    """
+    Simple implementation of the Giantbomb API that only offers the
+    GET /platforms/ call as a generator.
+
+    """
+
+    base_url = 'http://www.giantbomb.com/api'
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_platforms(self, sort=None, filter=None, field_list=None):
+        """Generator yielding platforms matching the given criteria.
+        If no limit is specified, thi will return all platforms.
+
+        """
+
+        # Do value-format conversions from common Python data types to what
+        # the API requires. Need to convert a dictionary of criteria
+        # into a comma-separated list of key-value paris.
+        params = {}
+        if sort is not None:
+            params['sort'] = sort
+        if field_list is not None:
+            params['field_list'] = ','.join(field_list)
+        if filter is not None:
+            params['filter'] = filter
+            parsed_filters = []
+            for key, value in filter.items():
+                parsed_filters.append('{0}:{1}'.format(key, value))
+            params['filter'] = ','.join(parsed_filters)
+
+        # Append API key to the list of parameters and have data being
+        # returned as JSON.
+        params['api_key'] = self.api_key
+        params['format'] = 'json'
+
+        incomplete_result = True
+        num_total_results = None
+        num_fetched_results = 0
+        counter = 0
+
+        while incomplete_result:
+            # Need to make multiple calls given Giantbomb's limit for items
+            # in a result set for this API is 100 items.
+            params['offset'] = num_fetched_results
+            result = requests.get(self.base_url + '/platforms/', params=params)
+            result = result.json()
+            if num_total_results is None:
+                num_total_results = int(result['number_of_total_results'])
+            num_fetched_results += int(result['number_of_page_results'])
+            if num_fetched_results >= num_total_results:
+                incomplete_result = False
+            for item in result['results']:
+                logging.debug("Yielding platform {0} of {1}".format(
+                    counter + 1,
+                    num_total_results))
+
+                # Convert values into a more useful format where appropriate.
+                if 'original_price' in item and item['original_price']:
+                    item['original_price'] = float(item['original_price'])
+
+                # Make this a generator
+                yield item
+                counter += 1
+
+def is_valid_datset(platform):
+    """Filters out datasets that can't be used because they are either
+    lacking a release date or an original price. For rendering the output
+    we also require the name and the abbreviation of the platform.
+
+    """
+    if 'release_date' not in platform or not platform['release_date']:
+        logging.warn("{0} has no release date".format(platform['name']))
+        return False
+    if 'original_price' not in platform or not platform['original_price']:
+        logging.warn("{0} has no original price".format(platform['name']))
+        return False
+    if 'name' not in platform or not platform['name']:
+        logging.warn("No platform name found for given dataset")
+        return False
+    if 'abbreviation' not in platform or not platform['abbreviation']:
+        logging.warn("{0} has no abbreviation".format(platform['name']))
+        return False
+    return True
 
 def main():
     """This functiom handles the actual logic of this script."""
